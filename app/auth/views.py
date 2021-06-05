24b07d07ea3cde9ba7f27c8a93255d9c2a0aac3f
NeretinD <neretin.dmitrie@yandex.ru>
2021-06-05 20:48:10 +0300

    Одиннадцатая глава

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,6 +8,16 @@ from app import db
 from ..email import send_email
 from flask_login import current_user
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
